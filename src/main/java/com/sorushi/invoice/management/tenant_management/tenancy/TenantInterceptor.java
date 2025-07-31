0385d53585adfc620c7a6f7aa6fb4c135ec3c723
Rushikesh <sorushikesh07@gmail.com>
2025-07-31 23:40:14 +0530

    Setup tenant interceptor to each request

---
+++ src/main/java/com/sorushi/invoice/management/tenant_management/tenancy/TenantInterceptor.java
@@ -0,0 +1,35 @@
+package com.sorushi.invoice.management.tenant_management.tenancy;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+
+public class TenantInterceptor implements HandlerInterceptor {
+
+  private static final String TENANT_HEADER = "X-Tenant-ID";
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws IOException {
+
+    String tenantId = request.getHeader(TENANT_HEADER);
+
+    if (StringUtils.hasText(tenantId)) {
+      TenantContext.setTenantId(tenantId);
+    } else {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write("Missing X-Tenant-ID header");
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) {
+    TenantContext.clear();
+  }
+}
