793ca3db583eb1c7473efd3bbe6b896aa9b5e214
Rushikesh <sorushikesh07@gmail.com>
2025-08-01 00:46:30 +0530

    Renamed table to users

--- src/main/java/com/sorushi/invoice/management/tenant_management/entity/User.java
+++ src/main/java/com/sorushi/invoice/management/tenant_management/entity/User.java
@@ -1,13 +1,11 @@
 package com.sorushi.invoice.management.tenant_management.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
